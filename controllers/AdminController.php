403874de522069c2159ddf2369e3dea507e6a7d2
miguelPretell12 <71430564+miguelPretell12@users.noreply.github.com>
2021-09-24 11:04:08 -0500

    Se modifico controllers

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -7,6 +7,8 @@ use MVC\Router;
 class AdminController  {
     public static function index(Router $router) {
         $cont = 1;
-        $router->render("Login/login",['cont'=>$cont]);
+        $router->render("Login/login",[
+            'cont'=>$cont
+        ]);
     }
 }
