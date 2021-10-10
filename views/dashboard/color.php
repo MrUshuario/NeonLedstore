2da6657da7dc92b17cab965e71d3319fed4fbbac
miguelPretell12 <71430564+miguelPretell12@users.noreply.github.com>
2021-10-10 10:57:44 -0500

    Se agrego un archivo para mostrar el CRUD

---
+++ views/dashboard/color.php
@@ -0,0 +1,67 @@
+<?php include 'layouts/loader.php'; ?>
+    <!-- Overlay For Sidebars -->
+    <div class="overlay"></div>
+    <!-- #END# Overlay For Sidebars -->
+    <?php include 'layouts/header.php'; ?>
+    <section>
+        <?php include 'layouts/nav.php'; ?>
+    </section>
+    <!-- CONTENT -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="block-header">
+                <button type="button" id="model-register" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalColor">
+                    Registrar Color
+                </button>
+
+                <div class="modal fade" id="modalColor" tabindex="-1" aria-labelledby="modalColor" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="modalColor">Guardar Color</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" id="cerrar"></button>
+                            </div>
+                            <div class="modal-body">
+                            <form action="post" id="formColor">
+                                <input type="hidden" id="id">
+                                <div class="mb-3">
+                                    <label for="nombre">Nombre de color</label>
+                                    <input type="text" class="form-control" id="nombre">
+                                </div>
+                                <div class="mb-3 flex">
+                                    <label for="rgb">Color</label>
+                                    <input type="color" class="form-color" id="rgb">
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                    <button type="submit" class="btn btn-primary" id="save">Guardar</button>
+                                </div>
+                            </form>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="m-1">
+                    <label for="buscarnombre">Buscar :</label>
+                    <input type="text" id="buscarnombre">
+                </div>
+                <div class="table-responsive tabla">
+                    <table class="table table-striped table-hover table-light table-fixed" id="tablacolor">
+                        <thead class="table-dark sticky" >
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Codigo de color</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody >
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+ 
+    <script src="build/js/ajaxColor.js"></script>
