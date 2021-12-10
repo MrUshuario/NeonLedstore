a54c2cfa022bc7448a0124b3c9d211da51e2f742
David Israel <israeldavi0904@gmail.com>
2021-12-10 12:39:53 -0500

    conector

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,8 +1,8 @@
 <?php
  function conexionDB(){
-     $db = new mysqli('localhost','root','','ghxumdmy_neonledstore');
-     mysqli_set_charset($db, "utf8");
-     if(!$db){
+     $db = new mysqli('localhost','Saint','Joseph23@','ghxumdmy_neonledstore');
+/*      mysqli_set_charset($db, "utf8");*/     
+    if(!$db){
          echo "no se conecto";
          exit;
      }
