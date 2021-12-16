dd450e0ee45763e387edd068df5d744758420904
NeoH33 <aldercaruajulcacercado@gmail.com>
2021-12-16 18:02:18 -0500

    agregando columas

--- views/principal/nosotros.php
+++ views/principal/nosotros.php
@@ -1,15 +1,47 @@
+<section class="pt-5 page-container bg-black">
+    <h1 class="text-white display-1 weight-medium m-3">Nosotros</h1> 
+    <div class="container">
+        <div class="row">
+            <div class="col-1 border border-1 text-light">1</div>
+            <div class="col-1 border border-1 text-light">2</div>
+            <div class="col-1 border border-1 text-light">3</div>
+            <div class="col-1 border border-1 text-light">4</div>
+            <div class="col-1 border border-1 text-light">5</div>
+            <div class="col-1 border border-1 text-light">6</div>
+            <div class="col-1 border border-1 text-light">7</div>
+            <div class="col-1 border border-1 text-light">8</div>
+            <div class="col-1 border border-1 text-light">9</div>
+            <div class="col-1 border border-1 text-light">10</div>
+            <div class="col-1 border border-1 text-light">11</div>
+            <div class="col-1 border border-1 text-light">12</div>
+        </div>
+    </div> 
+    <div class="container">
+            <div class="row">
+                <div class="col-lg-6 border border-1 text-light text-center">
+                    <h3 class="pt-lg-5">¿Quienes somos?</h3>
+                    <p class="fs-4">Somos una empresa que realizamos pedidos a diseño para que decores tu ambiente con las luces LED que más te gusten. Seguimo las ultimas tendencias para darte tu estilo a tu hogar y la imagen que quieres trnasmitir a tu negocio asi como ofrecemos una opcion de regalo original para familia, pareja y amigos.</p>
+                </div>
+                <div class="col-lg-6 border border-1 text-light">
+                    <img class="w-100" src="/build/img/nosotros/rs.webp" alt="ROCK STAR en luces de neon">
+                </div>
+            </div>
+        </div>  
+</section>
+
+
 <div class="pt-5 page-container bg-black">
         <div class="container">
             <h1 class="text-white display-1 weight-medium m-3">Nosotros</h1>
         <div class="contenido-grid">
             <div class="content-somos">
-                <h3>¿Quienes somos?</h3>
+                <h3 class="pt-5">¿Quienes somos?</h3>
                 <p>
                     Somos una empresa que realizamos pedidos a diseño para que decores tu ambiente con las luces LED que más te gusten. Seguimo las ultimas tendencias para darte tu estilo a tu hogar y la imagen que quieres trnasmitir a tu negocio asi como ofrecemos una opcion de regalo original para familia, pareja y amigos.
                 </p>
             </div>
             <div class="w-100 d-flex justify-content-center items-center">
-                <img class="w-100" src="/build/img/nosotros/rs.webp" alt="ROCK STAR en luces de neon">
+               
             </div>
             <div class="img-somos-7 w-100 d-flex justify-content-center items-center">
                 <img class="w-100" style="object-fit: scale-down;" src="/build/img/header/7.webp" alt="ALL WE HAVE IS NOW en luces de neon">
@@ -32,4 +64,5 @@
             </div>
         </div>
         </div>
+
     </div>
