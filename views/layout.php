e469e41d54248e69f23f70377d80cc99b01cd2b6
miguelPretell12 <71430564+miguelPretell12@users.noreply.github.com>
2022-01-04 11:12:00 -0500

    Se modifico los views

--- views/layout.php
+++ views/layout.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Panel de Control</title>
+    <link rel="shortcut icon" href="build/img/login/logo.webp" type="image/x-icon">
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
