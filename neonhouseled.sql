682afbdaf7d637f214692f91fff069ea7fdb7724
MrUsher <lenonpoma@gmail.com>
2022-01-25 09:36:44 -0500

    nueva bd

--- neonhouseled.sql
+++ neonhouseled.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0-rc1
+-- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 23-01-2022 a las 04:10:31
--- Versión del servidor: 8.0.28
--- Versión de PHP: 7.4.3
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-01-2022 a las 15:36:01
+-- Versión del servidor: 10.1.32-MariaDB
+-- Versión de PHP: 5.6.36
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -20,42 +21,24 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `neohouseled`
 --
-CREATE DATABASE IF NOT EXISTS `neohouseled` DEFAULT CHARACTER SET utf8mb4;
-USE `neohouseled`;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `User`
---
-
-CREATE TABLE tab_user (
-  user_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  user_email varchar(120) NOT NULL,
-  user_clave varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
---Volcado de datos para la tabla user--
-
-INSERT INTO tab_user (user_id, user_email, user_clave) VALUES
-
-(1, 'isabellam.montoya.im@gmail.com ' , 'esan2018');
-(2, '18100102@ue.edu.pe' , 'Itis1812');
 -- Estructura de tabla para la tabla `tab_categoria`
 --
 
-CREATE TABLE tab_categoria (
-  cat_id tinyint NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  cat_nombre varchar(40) NOT NULL,
-  cat_activo char(1) NOT NULL
+CREATE TABLE `tab_categoria` (
+  `cat_id` tinyint(4) NOT NULL,
+  `cat_nombre` varchar(40) NOT NULL,
+  `cat_activo` char(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla tab_categoria`
+-- Volcado de datos para la tabla `tab_categoria`
 --
 
-INSERT INTO tab_categoria (cat_id, cat_nombre,cat_activo) VALUES
+INSERT INTO `tab_categoria` (`cat_id`, `cat_nombre`, `cat_activo`) VALUES
 (1, 'Personajes', '1'),
 (2, 'Texto', '1'),
 (3, 'Objetos', '1'),
@@ -67,63 +50,192 @@ INSERT INTO tab_categoria (cat_id, cat_nombre,cat_activo) VALUES
 -- Estructura de tabla para la tabla `tab_cliente`
 --
 
-CREATE TABLE tab_cliente (
-  cli_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  cli_nombre varchar(120) NULL,
-  cli_apellidos varchar(120) NULL,
-  cli_email varchar(120) NULL,
-  cli_clave varchar(255) NULL,
-  cli_estado char(1) NULL
+CREATE TABLE `tab_cliente` (
+  `cli_id` int(11) NOT NULL,
+  `cli_nombre` varchar(120) DEFAULT NULL,
+  `cli_apellidos` varchar(120) DEFAULT NULL,
+  `cli_email` varchar(120) DEFAULT NULL,
+  `cli_clave` varchar(255) DEFAULT NULL,
+  `cli_estado` char(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tab_cliente`
+--
+
+INSERT INTO `tab_cliente` (`cli_id`, `cli_nombre`, `cli_apellidos`, `cli_email`, `cli_clave`, `cli_estado`) VALUES
+(1, 'Edgar', 'Poma', 'lenonpoma@gmail.com', '12345', '1');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Compra`
+-- Estructura de tabla para la tabla `tab_compra`
 --
 
-CREATE TABLE tab_compra (
-  cod_com int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  com_fecha datetime NULL,
-  precio_total float NULL,
-  cli_id int,
-  CONSTRAINT FK_cliente FOREIGN KEY (cli_id) REFERENCES tab_cliente(cli_id)
+CREATE TABLE `tab_compra` (
+  `cod_com` int(11) NOT NULL,
+  `com_fecha` datetime DEFAULT NULL,
+  `precio_total` float DEFAULT NULL,
+  `cli_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
-
-
---
--- Estructura de tabla para la tabla `Productos`
---
-
-CREATE TABLE tab_producto (
-  pro_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  cat_id tinyint,
-  pro_nombre varchar(100) NULL,
-  pro_descrpcion varchar(255) NULL,
-  pro_precio float NULL,
-  pro_tamaño varchar(20) NULL,
-  pro_activo char(1) NULL,
-  FOREIGN KEY (cat_id) REFERENCES tab_categoria(cat_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Estructura de tabla para la tabla `tab_compra_detalle`
 --
 
-CREATE TABLE tab_compra_detalle (
-  cod_det int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  cod_com int,
-  pro_id int,
-  det_cantidad int NULL,
-  det_color varchar(30) NULL,
-  FOREIGN KEY (cod_com) REFERENCES tab_compra(cod_com),
-  FOREIGN KEY (pro_id) REFERENCES tab_producto(pro_id)
+CREATE TABLE `tab_compra_detalle` (
+  `cod_det` int(11) NOT NULL,
+  `cod_com` int(11) DEFAULT NULL,
+  `pro_id` int(11) DEFAULT NULL,
+  `det_cantidad` int(11) DEFAULT NULL,
+  `det_color` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `tab_producto`
+--
+
+CREATE TABLE `tab_producto` (
+  `pro_id` int(11) NOT NULL,
+  `cat_id` tinyint(4) DEFAULT NULL,
+  `pro_nombre` varchar(100) DEFAULT NULL,
+  `pro_descrpcion` varchar(255) DEFAULT NULL,
+  `pro_precio` float DEFAULT NULL,
+  `pro_tamaño` varchar(20) DEFAULT NULL,
+  `pro_activo` char(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tab_user`
+--
+
+CREATE TABLE `tab_user` (
+  `id` int(11) NOT NULL,
+  `user` varchar(125) NOT NULL,
+  `pass` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tab_user`
+--
+
+INSERT INTO `tab_user` (`id`, `user`, `pass`) VALUES
+(1, 'adminNLS', '$2y$10$LgeWXAVuGEPOgd5LCSg.A.YF/PoSVhPBpTX4P9MApwRR6axty84dK'),
+(2, 'isabellam.montoya.im@gmail.com', 'esan2018'),
+(3, '18100102@ue.edu.pe', 'Itis1812');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tab_categoria`
+--
+ALTER TABLE `tab_categoria`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indices de la tabla `tab_cliente`
+--
+ALTER TABLE `tab_cliente`
+  ADD PRIMARY KEY (`cli_id`);
+
+--
+-- Indices de la tabla `tab_compra`
+--
+ALTER TABLE `tab_compra`
+  ADD PRIMARY KEY (`cod_com`),
+  ADD KEY `FK_cliente` (`cli_id`);
+
+--
+-- Indices de la tabla `tab_compra_detalle`
+--
+ALTER TABLE `tab_compra_detalle`
+  ADD PRIMARY KEY (`cod_det`),
+  ADD KEY `cod_com` (`cod_com`),
+  ADD KEY `pro_id` (`pro_id`);
+
+--
+-- Indices de la tabla `tab_producto`
+--
+ALTER TABLE `tab_producto`
+  ADD PRIMARY KEY (`pro_id`),
+  ADD KEY `cat_id` (`cat_id`);
+
+--
+-- Indices de la tabla `tab_user`
+--
+ALTER TABLE `tab_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tab_categoria`
+--
+ALTER TABLE `tab_categoria`
+  MODIFY `cat_id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `tab_cliente`
+--
+ALTER TABLE `tab_cliente`
+  MODIFY `cli_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `tab_compra`
+--
+ALTER TABLE `tab_compra`
+  MODIFY `cod_com` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tab_compra_detalle`
+--
+ALTER TABLE `tab_compra_detalle`
+  MODIFY `cod_det` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tab_producto`
+--
+ALTER TABLE `tab_producto`
+  MODIFY `pro_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tab_user`
+--
+ALTER TABLE `tab_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tab_compra`
+--
+ALTER TABLE `tab_compra`
+  ADD CONSTRAINT `FK_cliente` FOREIGN KEY (`cli_id`) REFERENCES `tab_cliente` (`cli_id`);
+
+--
+-- Filtros para la tabla `tab_compra_detalle`
+--
+ALTER TABLE `tab_compra_detalle`
+  ADD CONSTRAINT `tab_compra_detalle_ibfk_1` FOREIGN KEY (`cod_com`) REFERENCES `tab_compra` (`cod_com`),
+  ADD CONSTRAINT `tab_compra_detalle_ibfk_2` FOREIGN KEY (`pro_id`) REFERENCES `tab_producto` (`pro_id`);
+
+--
+-- Filtros para la tabla `tab_producto`
+--
+ALTER TABLE `tab_producto`
+  ADD CONSTRAINT `tab_producto_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `tab_categoria` (`cat_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
