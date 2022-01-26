ea5c0278a73bcf77be92560a78fceaa159f67870
Isa18121999 <97621420+Isa18121999@users.noreply.github.com>
2022-01-26 09:38:36 -0500

    Update neohouseled.sql

--- neohouseled.sql
+++ neohouseled.sql
@@ -130,8 +130,8 @@ CREATE TABLE `tab_user` (
 INSERT INTO `tab_user` (`id`, `user`, `pass`) VALUES
 (1, 'adminNLS', '$2y$10$LgeWXAVuGEPOgd5LCSg.A.YF/PoSVhPBpTX4P9MApwRR6axty84dK'),
 (2, 'isabellam.montoya.im@gmail.com', '37342ac90656f687bc8dd96f6243f4a7'),
-(3, '18100102@ue.edu.pe', 'e70f7f031bed60c54604bf0a88732e59
-');
+(3, '18100102@ue.edu.pe', 'e70f7f031bed60c54604bf0a88732e59') ;
+
 
 --
 -- Índices para tablas volcadas
