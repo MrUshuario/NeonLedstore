8523b29d258541dcc3a89489322ce749af6a801e
Isa18121999 <97621420+Isa18121999@users.noreply.github.com>
2022-02-02 10:37:23 -0500

    Update conexion.php

--- Carrito/conexion.php
+++ Carrito/conexion.php
@@ -1,24 +1,24 @@
 
+
 <?php
-	$server="127.0.0.1 ";
-	$username="isabella181299";
-	$password="Esan2018";
-	$db='neohouseled';
-	
-	$con=mysqli_connect($server,$username,$password,$db);
-	
-	if (mysqli_connect_errno()) {
-       printf("Falló la conexión: %s\n", mysqli_connect_error());
-       exit();
+
+function conexionDB()
+
+{
+
+    $db = new mysqli('localhost', 'root', '', 'neohouseled');
+
+    mysqli_set_charset($db, "utf8");
+
+    if (!$db) {
+
+        echo "no se conecto";
+
+        exit;
+
     }
 
-	
-    if (!mysqli_set_charset($con,"utf8")) {
-        printf("Error cargando el conjunto de caracteres utf8: %s\n", mysqli_error($con));
-        exit();
-    } else {
-        mysqli_character_set_name($con);
-    }
+    return $db;
 
-    //mysqli_close($con);
+}
 ?>
