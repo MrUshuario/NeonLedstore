25a875b24c615d1c32cd17afd0fc66eac14ac24f
RenzoCO <rcounac@gmail.com>
2022-02-08 12:57:40 -0500

    restringir numero celular

--- views/principal/contacto.php
+++ views/principal/contacto.php
@@ -13,9 +13,9 @@
                                 <div class="container">
                                 <div class="row">
                                     <div class="col-6 my-auto mx-auto">
-                                    <input class="p-2 neontextlanding  border-0 border bg-transparent rounded w-100 me-1 me-sm-4 my-3 d-block text-white" type="text" name="nombre" id="nombre" placeholder="Nombre" > <br>
-                                    <input class="p-2 neontextlanding  border-0 border bg-transparent rounded w-100 me-1 me-sm-4 my-3 d-block text-white" type="text" name="apellidos" id="apellidos" placeholder="Apellidos" > <br>
-                                    <input class="p-2 neontextlanding  border-0 border bg-transparent rounded w-100 me-1 me-sm-4 my-3 d-block text-white" type="email" name="correo" id="correo" placeholder="Correo Electronico" > <br>
+                                    <input class="p-2 neontextlanding  border-0 border bg-transparent rounded w-100 me-1 me-sm-4 my-3 d-block text-white" type="text" name="nombre" id="nombre" placeholder="Nombre" required> <br>
+                                    <input class="p-2 neontextlanding  border-0 border bg-transparent rounded w-100 me-1 me-sm-4 my-3 d-block text-white" type="text" name="apellidos" id="apellidos" placeholder="Apellidos" required> <br>
+                                    <input class="p-2 neontextlanding  border-0 border bg-transparent rounded w-100 me-1 me-sm-4 my-3 d-block text-white" type="email" name="correo" id="correo" placeholder="Correo Electronico" required> <br>
                                     <input class="p-2 neontextlanding  border-0 border bg-transparent rounded w-100 me-1 me-sm-4 my-3 d-block text-white" type="number" name="telefono" id="telefono" pattern="[0-9]{9,12}" placeholder="N° de celular"> <br>
                                     </div>
 
