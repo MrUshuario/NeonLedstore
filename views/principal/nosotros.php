17cb02b08bfcc02d8d42504683273ce6de83f3d8
P. La Plata <73401234+plaplata@users.noreply.github.com>
2022-02-14 09:53:51 -0500

    fuente aplicada a botones y placeholders

--- views/principal/nosotros.php
+++ views/principal/nosotros.php
@@ -3,7 +3,7 @@
             <h1 class="text-white display-1 weight-medium m-3">Nosotros</h1>
         <div class="contenido-grid">
             <div class="content-somos">
-                <h3>¿Quienes somos?</h3>
+                <h3>¿Quiénes somos?</h3>
                 <p>
                     Somos una empresa que realizamos pedidos a diseño para que decores tu ambiente con las luces LED que más te gusten. Seguimo las ultimas tendencias para darte tu estilo a tu hogar y la imagen que quieres trnasmitir a tu negocio asi como ofrecemos una opcion de regalo original para familia, pareja y amigos.
                 </p>
@@ -15,14 +15,15 @@
                 <img class="w-100" style="object-fit: scale-down;" src="/build/img/header/7.webp" alt="ALL WE HAVE IS NOW en luces de neon">
             </div>
             <div class="content-ofrecer">
+                <br><br><br>
                 <h3>¿Qué ofrecemos?</h3>
                 <p>
                     Neón Led Store trabaja con un sistema de luz flexible, duradero y de bajo consumo con una granversatilidad para diferentes usos.
-                    Ofrecemos una ventaja significativa como medio publicitario y decorativo. Un sistema útil para captar la atención de sus clientes, generar experiencias y destacar tu marca
+                    Ofrecemos una ventaja significativa como medio publicitario y decorativo. Un sistema útil para captar la atención de sus clientes, generar experiencias y destacar tu marca.
                 </p>
             </div>
             <div class="content-expresar">
-                <h3>Expresate diferente con nosotros</h3>
+                <h3>Exprésate diferente con nosotros</h3>
                 <p>
                     Creamos un ambiente que brinde una experiencia memorable para los usuarios conservando los conceptos y el valor de la marca con la quese trabaja.
                 </p>
