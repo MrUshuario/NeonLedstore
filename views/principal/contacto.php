e35c393928782c6c799bbae874a0a7d0f726f84d
RenzoCO <rcounac@gmail.com>
2022-02-21 10:01:09 -0500

    Cambios overlay-grid de Productos

--- views/principal/contacto.php
+++ views/principal/contacto.php
@@ -45,38 +45,38 @@
     <div class="container">
         <section class="row pb-5">
             
-                <div class="container">
-                    <div class="row text-white mx-1 mx-sm-5 pt-3 mb-2 d-flex justify-content-end">
+            <div class="container">
+                <div class="row text-white mx-1 mx-sm-5 pt-3 mb-2 d-flex justify-content-end">
                     <div class="col-lg-5 border neontabla redesociales">
-                            <h2 class= "neones">Redes Sociales</h2>
-                                <div class="container">
+                        <h2 class= "neones">Redes Sociales</h2>
+                            <div class="container">
                                 <div class="row">
                                     <div class="col-12 my-auto mx-auto">
-                                   <a href="https://www.instagram.com/neon_led_store/?hl=es"> <i class="fab fa-instagram iconoFont"></i> <input class="p-2 bg-transparent neontextlanding  border-0 border rounded me-1 me-sm-4 my-3 d-block text-white" type="text" name="Instagram" id="Instagram" value="Neon_Led_Store" readonly> </a><br><br><br>
-                                   <a href="https://www.facebook.com/Neon-Led-store-100244098454782"> <i class="fab fa-facebook iconoFont"></i> <input class="p-2 bg-transparent neontextlanding  border-0 border  rounded me-1 me-sm-4 my-3 d-block text-white" type="text" name="facebook" id="facebook" value="Neon Led Store" readonly> </a>
+                                        <a href="https://www.instagram.com/neon_led_store/?hl=es"> <i class="fab fa-instagram iconoFont"></i> <input class="p-2 bg-transparent neontextlanding  border-0 border rounded me-1 me-sm-4 my-3 d-block text-white" type="text" name="Instagram" id="Instagram" value="Neon_Led_Store" readonly> </a><br><br><br>
+                                        <a href="https://www.facebook.com/Neon-Led-store-100244098454782"> <i class="fab fa-facebook iconoFont"></i> <input class="p-2 bg-transparent neontextlanding  border-0 border  rounded me-1 me-sm-4 my-3 d-block text-white" type="text" name="facebook" id="facebook" value="Neon Led Store" readonly> </a>
                                     </div>
                                 </div>
-                                </div>
-                                </div>
+                            </div>
+                    </div>
                                 
-                                <div class="col-lg-2 border rellenocontact">
-                                    <h2 class= "neones">relleno</h2>
-                                </div>
+                    <div class="col-lg-2 border rellenocontact">
+                        <h2 class= "neones">relleno</h2>
+                    </div>
 
-                        <div class="col-lg-5 border neontabla">
+                    <div class="col-lg-5 border neontabla">
                             <h2 class= "neones">Datos</h2>
-                                <div class="container">
-                                <div class="row">
-                                    <div class="col-12 my-auto mx-auto">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-12 my-auto mx-auto">
                                     <input class="p-2 neontextlanding bg-transparent border-0 border  rounded w-100 me-1 me-sm-4 my-3 d-block text-white" type="text" name="contacto" id="contacto" value="Contacto: 994 078 320" readonly> <br>
                                     <input class="p-2 neontextlanding bg-transparent border-0 border  rounded w-100 me-1 me-sm-4 my-3 d-block text-white" type="text" name="gmail" id="gmail" value="email: info.neonledstore@gmail.com" readonly> <br>
                                     <input class="p-2 neontextlanding bg-transparent border-0 border  rounded w-100 me-1 me-sm-4 my-3 d-block text-white" type="text" name="dirreción" id="dirreción" value="Jr.paruro 1401 tda 130 CC Paruro" readonly> <br>
-                                    </div>
                                 </div>
-                                </div>
-                    </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
+            </div>
            
         </section>
     </div>
--- views/principal/productos.php
+++ views/principal/productos.php
@@ -1,272 +1,262 @@
-<!-- <link rel="stylesheet" href="grid.css">  -->
-
 <div class="productos page-container bg-black">
-    <main class="main mb-3 mb-sm-5">
+    <main class="main">
         <div class="contenedorMain container pt-1 pt-sm-5">
             <div class="row text-white d-flex justify-content-around">
                 <div class="d-flex flex-column col-6 col-sm-4">
                     <h1 class="title-product py-1 py-sm-3 text-start fw-bold">PRODUCTO</h1>
-                    <div class="h-100 w-100 d-flex items-center">
-                        <img class="img-product w-100" src="/build/img/productos/brideToBe.webp" alt="brideToBe">
-                    </div>
+                        <div class="h-100 w-100 d-flex items-center">
+                            <img class="img-product w-100" src="/build/img/productos/brideToBe.webp" style="border-radius: 15px">
+                        </div>
                 </div>
                 <div class="contenedorTexto col-6 col-sm-4 pt-5 pt-sm-3 ">
-                    <h2 class="subtitulo text-center fw-bold">NEÓN DESIGN</h2>
-                    <div class="h-100 d-flex align-items-center">
-                        <aside class="">
-                            <p class="parrafo">Actualmente los diseños de neon led se han vuelto una tendencia interesante y
-                                diferente para cualquier evento, negocio o para la decoración de tu habitación.
+                    <h1 class="subtitulo text-center fw-bold">NEÓN DESIGN</h2>
+                        <div class="h-100 d-flex align-items-center">
+                            <aside class="">
+                                <p class="parrafo">Actualmente los diseños de neon led se han vuelto una tendencia interesante y
+                                diferente para cualquier evento, negocio o para la decoración de tu hogar.
                                 En NEON LED STORE realizamos paneles 100% personalizados al estilo de tu marca
-                                o para lo que tu evento nececit. Recuerda que puedes cotizar cualquier diseño con
-                            <span><a class="enlaceNosotros" href="/nosotros">nosotros.</a></span>
-                            </p>
-                        </aside>
+                                o para lo que tu evento necesita. Recuerda que puedes cotizar cualquier diseño con
+                                    <span><a class="enlaceNosotros" href="/nosotros">nosotros.</a></span>
+                                </p>
+                            </aside>
                         </div>
-                    </div>
                 </div>
             </div>
-        </main>
-<!--FILTRO-->
-<article class="article1 pb-5">
+        </div>
+    </main>
+
+
+    <div class="container" style="display: inline">
+        <section class="row pb-5"> 
+
             <div class="container">
-                <div class="row">
-                    <div class="col-12 col-sm-6 col-md-5 col-lg-4 col-xl-3 text-white">
+                <div class="row text-white mx-1 mx-sm-5 pt-3 mb-2 d-flex">
+                    <!-- FILTRO -->    
+                    <div class="col-lg-3">
+                        
                         <div class="filtros border border-light border-4 border-bottom-0 border-start-0  d-flex align-items-center fw-bold">
                             Filtros
                         </div>
                         <div class="accordion" id="accordionPanelsStayOpenExample">
                             <div class="cajaFiltroCategoria accordion-item border-bottom-0 border-start-0 border-4 border-light rounded-0 bg-black">
-                                <h2 class="accordion-header w-100 text-white d-flex justify-content-center items-center" id="panelsStayOpen-headingOne">
+                                <h2 class="accordion-header border border-light w-100 text-white d-flex justify-content-center items-center" id="panelsStayOpen-headingOne">
                                     <button class="accordion-button collapsed filtroCategoria float-left text-start bg-black text-white fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
                                         Categoría
                                     </button>
                                 </h2>
                                 <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
-                                    <div class="accordion-body" id="filtro_categoria">
-                                        <!--IMPRIME TODO LO DE CATEGORIA-->
+                                    <div class="accordion-body border" id="filtro_categoria">
+                                    <!--IMPRIME TODO LO DE CATEGORIA-->
                                     </div>
                                 </div>
                             </div>
-
+                        
+                        
                             <div class="cajaFiltroPrecio accordion-item border border-4 border-bottom-0 border-light border-start-0 bg-black">
                                 <h2 class="accordion-header w-100 text-white d-flex justify-content-center items-center" id="panelsStayOpen-headingTwo">
-                                    <button class="accordion-button collapsed filtroPrecio float-left text-start bg-black text-white fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                                    <button class="accordion-button border border-light collapsed filtroPrecio float-left text-start bg-black text-white fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
                                         Precio
                                     </button>
                                 </h2>
                                 <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-                                    <div class="accordion-body">
+                                    <div class="accordion-body border">
                                         <div class="range bg-dark p-2">
                                             <div class="container mt-4">
                                                 <input type="range" id="inputrange" class="js-range-slider" name="my_range" value="" tabindex="-1" readonly="">
                                             </div>
-                                            <p class="text-center">Precio S/.140 - S/.860</p>
-                                            <div class="row d-flex justify-content-center">
-                                                <button id="optrange" name="btn_range" class="precio btn btn-secondary col-6">Filtrar</button>
+                                                <p class="text-center">Precio S/.140 - S/.860</p>
+                                                <div class="row d-flex justify-content-center">
+                                                    <button id="optrange" name="btn_range" class="precio btn btn-secondary col-6">Filtrar</button>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                      
+                        </div>                  
+                
+                        <!--FIN FILTRO-->
 
-                       
-                    </div>
-                </div>
-                    <!---
-                        <div class='productCard col-6 p-1 col-lg-4 text-white align-self-end'>
-                            <div class=''>
-                                <a href='producto'>
-                                    <div class='d-block d-flex justify-content-center'>
-                                        <picture class='mw-75'>
-                                            <source media='(min-width:600px)'  srcset=''>
-                                            <img class='' src='' alt='corazonesAngelCaido'>
-                                        </picture>
+                        <div class="col-lg-9">     
+                            <div class="container">
+                                <div class="row">          
+                                    <h2 class="title-product py-sm-3 text-white text-center fw-bold">GALERÍA DE PRODUCTOS</h2>               
+
+
+                                <ma class="ma">
+                                    <div class="galeria">
+
+                                        <div class="contenedor-imagenes">
+
+                                            <div class="imagen">
+
+                                                <img src="build/img/productos/brideToBe.webp" alt="">
+
+                                                    <div class="overlay">
+
+                                                        <h2>ver detalle </h2>
+
+                                                    </div>
+
+                                            </div>
+
+                                            <div class="imagen">
+
+                                                <img src="build/img/productos/brideToBe.webp" alt="">
+
+                                                    <div class="overlay">
+
+                                                        <h2>ver detalle </h2>
+
+                                                    </div>
+
+                                            </div>
+
+                                            <div class="imagen">
+
+                                                <img src="build/img/productos/brideToBe.webp" alt="">
+
+                                                    <div class="overlay">
+
+                                                        <h2>ver detalle </h2>
+
+                                                    </div>
+
+                                            </div>
+
+                                            <div class="imagen">
+
+                                                <img src="build/img/productos/brideToBe.webp" alt="">
+
+                                                    <div class="overlay">
+
+                                                        <h2>ver detalle </h2>
+
+                                                    </div>
+
+                                            </div>
+
+                                            <div class="imagen">
+
+                                                <img src="build/img/productos/brideToBe.webp" alt="">
+
+                                                    <div class="overlay">
+
+                                                        <h2>ver detalle </h2>
+
+                                                    </div>
+
+                                            </div>
+
+                                            <div class="imagen">
+
+                                                <img src="build/img/productos/brideToBe.webp" alt="">
+
+                                                    <div class="overlay">
+
+                                                        <h2>ver detalle </h2>
+
+                                                    </div>
+
+                                            </div>
+
+                                            <div class="imagen">
+
+                                                <img src="build/img/productos/brideToBe.webp" alt="">
+
+                                                    <div class="overlay">
+
+                                                        <h2>ver detalle </h2>
+
+                                                    </div>
+
+                                            </div>
+
+                                            <div class="imagen">
+
+                                                <img src="build/img/productos/brideToBe.webp" alt="">
+
+                                                    <div class="overlay">
+
+                                                        <h2>ver detalle </h2>
+
+                                                    </div>
+
+                                            </div>
+
+                                            <div class="imagen">
+
+                                                <img src="build/img/productos/brideToBe.webp" alt="">
+
+                                                    <div class="overlay">
+
+                                                        <h2>ver detalle </h2>
+
+                                                    </div>
+
+                                            </div>
+
+                                            <div class="imagen">
+
+                                                <img src="build/img/productos/brideToBe.webp" alt="">
+
+                                                    <div class="overlay">
+
+                                                        <h2>ver detalle </h2>
+
+                                                    </div>
+
+                                            </div> 
+
+                                            <div class="imagen">
+
+                                                <img src="build/img/productos/brideToBe.webp" alt="">
+
+                                                    <div class="overlay">
+
+                                                        <h2>Ver detalle </h2>
+
+                                                    </div>
+
+                                            </div>
+
+                                            <div class="imagen">
+
+                                                <img src="build/img/productos/brideToBe.webp" alt="">
+
+                                                    <div class="overlay">
+
+                                                        <h2>ver detalle </h2>
+
+                                                    </div>
+
+                                            </div> 
+
+                                        </div>
+
                                     </div>
-                                </a>
-                                <h3 class='productCardTitle h3 text-center'></h3>
-                                <div class='d-flex justify-content-center w-100 mb-3 mt-3'>
-                                    <div class='w-75 product-led' style='height: 5px;'></div>
-                                </div>
-                                <div class='d-block col-8 mx-auto mt-2'>
-                                    <div class='gap-2 d-flex justify-content-center'></div>
-                                    <p class='productCardPrice weight-bolder h4 text-center'></p>
-                                </div>
+                                </ma>
                             </div>
                         </div>
-                        -->
-        </article>
-
-<article class="article2 p-1 p-sm-5">
-
-    <div class="container">
-        <div class="row text-white d-flex justify-content-around">
-                <div class="d-flex flex-column col-3 col-sm-10">
-                    <h2 class="title-product py-1 py-sm-3 text-start fw-bold">GALERÍA DE PRODUCTOS</h2>               
-
-
-        <ma class="ma">
-
-            <div class="galeria">
-
-                <div class="contenedor-imagenes">
-
-                    <div class="imagen">
-
-                        <img src="build/img/productos/brideToBe.webp" alt="">
-
-                    <div class="overlay">
-
-                        <h2>ver detalle </h2>
-
+                    </div>
                 </div>
 
             </div>
-
-            <div class="imagen">
-
-                <img src="build/img/productos/brideToBe.webp" alt="">
-
-                    <div class="overlay">
-
-                        <h2>ver detalle </h2>
-
-                    </div>
-
+                
             </div>
 
-            <div class="imagen">
-
-                <img src="build/img/productos/brideToBe.webp" alt="">
-
-                    <div class="overlay">
-
-                        <h2>ver detalle </h2>
-
-                    </div>
-
-            </div>
-
-            <div class="imagen">
-
-                <img src="build/img/productos/brideToBe.webp" alt="">
-
-                    <div class="overlay">
-
-                        <h2>ver detalle </h2>
-
-                    </div>
-
-            </div>
-
-            <div class="imagen">
-
-                <img src="build/img/productos/brideToBe.webp" alt="">
-
-                    <div class="overlay">
-
-                        <h2>ver detalle </h2>
-
-                    </div>
-
-            </div>
-
-            <div class="imagen">
-
-                <img src="build/img/productos/brideToBe.webp" alt="">
-
-                    <div class="overlay">
-
-                        <h2>ver detalle </h2>
-
-                    </div>
-
-            </div>
-
-            <div class="imagen">
-
-                <img src="build/img/productos/brideToBe.webp" alt="">
-
-                    <div class="overlay">
-
-                        <h2>ver detalle </h2>
-
-                    </div>
-
-            </div>
-
-            <div class="imagen">
-
-                <img src="build/img/productos/brideToBe.webp" alt="">
-
-                    <div class="overlay">
-
-                        <h2>ver detalle </h2>
-
-                    </div>
-
-            </div>
-
-            <div class="imagen">
-
-                <img src="build/img/productos/brideToBe.webp" alt="">
-
-                    <div class="overlay">
-
-                        <h2>ver detalle </h2>
-
-                    </div>
-
-            </div>
-
-            <div class="imagen">
-
-                <img src="build/img/productos/brideToBe.webp" alt="">
-
-                    <div class="overlay">
-
-                        <h2>ver detalle </h2>
-
-                    </div>
-
-            </div> 
-
-            <div class="imagen">
-
-                <img src="build/img/productos/brideToBe.webp" alt="">
-
-                    <div class="overlay">
-
-                        <h2>Ver detalle </h2>
-
-                    </div>
-
-            </div>
-
-            <div class="imagen">
-
-                <img src="build/img/productos/brideToBe.webp" alt="">
-
-                    <div class="overlay">
-
-                        <h2>ver detalle </h2>
-
-                    </div>
-
-            </div> 
-
-        </div>
-
-        </div>
-
-        </ma>
-
-        </div>
+        </section>
     </div>
-</article>
+
+
+</div>        
+
+
+
+
+
+
+
 
 <script src="build/js/ajax/ajax.filtrado.js"></script>
-
- 
-
- 
