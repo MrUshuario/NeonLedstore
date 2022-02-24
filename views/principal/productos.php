8f702f2d07b6f9d158d497aa4f60f8e270639f07
RenzoCO <rcounac@gmail.com>
2022-02-24 13:01:13 -0500

    Ajustes Modal Producto version 1

--- views/principal/productos.php
+++ views/principal/productos.php
@@ -89,7 +89,7 @@
                             
                             <div class="galproductos arreglo">
                                 <div class="container" id="productoimprimir">
-                                   <!--IMPRIMIRA IMAGENES-->
+                                   <!--IMPRIME IMAGENES-->
                                 </div>
                             </div>   
 
@@ -112,27 +112,29 @@
                 <main class="main rounded-25 row pb-5 d-flex justify-content-center">
                 <!--Modificar para que quede como lo piden -->
                     <div class="mainConte col-8 my-5 pb-3">
-                        <h1 class="my-3 fs-2 neones p-50">¿Quieres tener un negocio visualmente atractivo?</h1>
+                        <h1 class="my-3 fs-2 neones p-50">aqui va Nombre Producto</h1>
                         <div class="row d-flex justify-content-center">
 
-                        <form class="formContact landingPageForm col-12 col-sm-10" method="POST">
-                            <input type="hidden" id="consulta" value="formNegocio">
+                        <form class="modalPro col-12 col-sm-10" method="POST">
                             <div class="modal-field">
-                                <input type="hidden" class="neontextlanding bg-transparent text-center form-control my-3 border-0" name="contador" id="contador" value=1>
+                                <label>Precio: </label>
+                                <input class="neontextlanding bg-transparent text-center form-control my-3 border-0">
                             </div>
                             <div class="modal-field">
-                                <input type="text" class="neontextlanding bg-transparent text-center form-control my-3 border-0" name="nombre" id="nombre" placeholder="Nombres" required>
+                                <label>Tamaño: </label>
+                                <input type="" class="neontextlanding bg-transparent text-center form-control my-3 border-0">
                             </div>
                             <div class="modal-field">
-                                <input type="text" class="neontextlanding bg-transparent text-center form-control my-3 border-0" name="apellido" id="apellidos" placeholder="Apellidos" required>
+                                <label>Color: </label>
+                                    <select class="p-2 neontextlanding bg-transparent rounded w-100 me-1 me-sm-4 my-3 d-block text-black" name="consulta" id="consulta" placeholder="Elija Opcion"><br>             
+                                        <option disabled selected="">--- Seleccione ---</option>
+                                        <option id="pro-color" value="color1">Color 1</option>
+	                                    <option id="pro-color" value="color2">Color 2</option>
+	                                    
+                                    </select>
                             </div>
-                            <div class="modal-field">
-                                <input type="email" class="neontextlanding bg-transparent text-center form-control my-3 border-0" name="correo" id="correo" placeholder="Correo Electrónico" required>
-                            </div>
-                            <div class="modal-field">
-                                <input type="number" class="neontextlanding bg-transparent text-center form-control my-3 border-0 mb-3" name="telefono" id="telefono" placeholder="Número de celular" required>
-                            </div> <!--crea tu propio boton, lo agregas en NEON.css -->
-                            <button type="submit" class="neonbottonlanding btn btn-primary my-sm-3 border-0"  id="btn-modal-asesoria Negocio" onclick="Negocio();">Pedir asesoria</button>
+                            <a href="/ProductoDetallado" class="neonbtnVermas btn btn-primary my-sm-3 border-0"  style="width: auto;">Ver más</a>
+                            
                         </form>
                     </div>     
                     </div>
@@ -140,7 +142,7 @@
                 </main>
                 </div>
             </div>
-            </div>
+        </div>
 
 
 </div>        
