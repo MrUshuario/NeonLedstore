85dcbc97794dd8f56e6305ee9c5c420a27c4b97b
Paul Perea <paul_18_baby@hotmail.com>
2022-03-01 10:33:45 -0500

    Cambio carrucel

--- views/principal/ProductoDetallado.php
+++ views/principal/ProductoDetallado.php
@@ -18,5 +18,28 @@
                                 <option id="pro-color" value="2">AZUL</option>
                                 <option id="pro-color" value="3">MULTICOLOR</option>
                             </select>
-                        </form>    
+        </form>    
+    <br>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="/build/img/landingPage/lp-hogar/h1.jpg" class="d-block w-100" id="imgCarrusHogar1" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="/build/img/landingPage/lp-hogar/h2.jpg" class="d-block w-100" id="imgCarrusHogar2" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="/build/img/landingPage/lp-hogar/h3.jpg" class="d-block w-100" id="imgCarrusHogar3" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
+</div>
