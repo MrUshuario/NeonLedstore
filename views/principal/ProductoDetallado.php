7d1a152b25cd428510ec617a9d78c133b739602d
MrUsher <lenonpoma@gmail.com>
2022-03-03 00:42:00 -0500

    arregladoproductodetallado

--- views/principal/ProductoDetallado.php
+++ views/principal/ProductoDetallado.php
@@ -1,26 +1,4 @@
 <div class="pt-5 page-container bg-black">
-        <!-- <div class="container">
-            <h1 class="text-white display-1 weight-medium m-3">Producto</h1>
-        <div class="contenido-grid">
-        
-        
-        <form class="formMod col-12 col-sm-10">
-                            <input type="hidden" id="id" value="formNegocio">
-                            <div class="modal-field">
-                                <label type="text" id="pro_precio" class="font-weight-bold text-white bg-transparent text-center form-control my-3 border-0"></label>
-                            </div>
-                            <div class="modal-field">
-                                <label type="text" id="pro_tamano" class="font-weight-bold text-white bg-transparent text-center form-control my-3 border-0"></label>
-                            </div>
-                            
-                            <select name="color" class="neontextlanding border-0 border bg-transparent rounded w-100 me-1 me-sm-4 my-3 d-block">
-                                <option id="pro-color" value="1">ROJO</option>
-                                <option id="pro-color" value="2">AZUL</option>
-                                <option id="pro-color" value="3">MULTICOLOR</option>
-                            </select>
-        </form>    
-    
-    <br> -->
 
 
 
@@ -55,28 +33,46 @@
         <!-- Recopilando la parte del compañero -->
     
         <div class="row text-white   pt-3 mb-2 d-flex justify-content-end tamano_des">
-                            <div class="formulario col-12 col-sm-12  border neontabla largoCom">
-                                <div class="container">
-                                    <div class="row">
-                                        <p> 
-                                            Aqui tienes que colocar tu codigo ordenado
-                                        </p>
+            <div class="formulario col-12 col-sm-12  border neontabla largoCom">
+                <div class="container">
+                    <div class="row">
+                        <div class="container">
+                        <h1 class="text-white display-1 weight-medium m-3">Producto</h1>
+                            <div>
+                                <form class="formMod col-12 col-sm-10">
+
+                                    <input type="hidden" id="id" value="formNegocio">
+
+                                    <div class="modal-field">
+                                    <label type="text" id="pro_precio" class="font-weight-bold text-white bg-transparent text-center form-control my-3 border-0"></label>
                                     </div>
-                                </div>
-                            </div>
+
+                                    <div class="modal-field">
+                                    <label type="text" id="pro_tamano" class="font-weight-bold text-white bg-transparent text-center form-control my-3 border-0"></label>
+                                    </div>
+                                
+                                    <select name="color" class="neontextlanding border-0 border bg-transparent rounded w-100 me-1 me-sm-4 my-3 d-block">
+                                    <option id="pro-color" value="1">ROJO</option>
+                                    <option id="pro-color" value="2">AZUL</option>
+                                    <option id="pro-color" value="3">MULTICOLOR</option>
+                                    </select>     
+                                    <i class="fa-solid fa-dolly"></i>
+                                    <a href="/productos" class="neonbottonlanding btn btn-primary my-sm-3 border-0" style="width: auto; height:auto font-size: 18px;"> Comprar </a>
+                                </form>
+                                <hr>
+                                <br>
+                                <p><i class="fa-solid fa-text-height"></i>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat.</p>
+                                <i class="fa-solid fa-angles-left"></i><a href="/productos" class="neonbottonlanding btn btn-primary my-sm-3 border-0" style="width: auto; height:auto font-size: 18px;"> Volver </a>
+                            </div>   
+                        </div>
+                    </div>
+                </div>
+            </div>                        
         </div>
-    </div>
 
 </div>
 
-<!-- <i class="fa-solid fa-dolly"></i>   <a href="/productos" class="neonbottonlanding btn btn-primary my-sm-3 border-0" style="width: auto; height:auto font-size: 18px;"> Comprar </a>
-                        </form>
-                        <hr> <br>
-                        <p><i class="fa-solid fa-text-height"></i>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat.</p>
-                        <i class="fa-solid fa-angles-left"></i><a href="/productos" class="neonbottonlanding btn btn-primary my-sm-3 border-0" style="width: auto; height:auto font-size: 18px;"> Volver </a>
-                    </div>   
-    </div></div></div></div> -->
