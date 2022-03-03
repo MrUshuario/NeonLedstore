23972609ee646f70050b99918615a339152fe1b3
Paul Perea <paul_18_baby@hotmail.com>
2022-03-03 11:25:44 -0500

    Subiendo archivos 20%

--- views/principal/ProductoDetallado.php
+++ views/principal/ProductoDetallado.php
@@ -1,62 +1,7 @@
 <div class="pt-5 page-container bg-black">
 
-<<<<<<< HEAD
-<div class="arreglos">
-
-    <div id="carouselExampleIndicators" class="carousel slide tamano-carrus" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img src="/build/img/landingPage/lp-hogar/h1.jpg" class="d-block w-100 tam" id="imgCarrusHogar1" alt="...">
-                </div>
-                <div class="carousel-item">
-                <img src="/build/img/landingPage/lp-hogar/h2.jpg" class="d-block w-100 tam" id="imgCarrusHogar2" alt="...">
-                </div>
-                <div class="carousel-item">
-                <img src="/build/img/landingPage/lp-hogar/h3.jpg" class="d-block w-100 tam" id="imgCarrusHogar3" alt="...">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
-        
-
-    <div class="separador"></div>
-    <div class="detalle">
-        <div class="container">
-            <h1 class="text-white display-1 weight-medium m-3">Producto</h1>
-        <div class="conten">
-            <p><label>descripcion del producto</label></p>
-        <i class="fa-solid fa-hand-holding-dollar"></i> <label type="text" id="pro_precio">S/ 300</label>
-        <!--CONTENIDO-->
-        <form class="formMod col-12 col-sm-10">                            
-                            <select name="color" class="neontextlanding border-0 border bg-transparent rounded w-100 me-1 me-sm-4 my-3 d-block">
-                                <option id="pro-color" value="1">ROJO</option>
-                                <option id="pro-color" value="2">AZUL</option>
-                                <option id="pro-color" value="3">MULTICOLOR</option>
-                            </select>
-                            <i class="fa-solid fa-dolly"></i>   <a href="/productos" class="neonbottonlanding btn btn-primary my-sm-3 border-0" style="width: auto; height:auto font-size: 18px;"> Comprar </a>
-                        </form>
-                        <hr> <br>
-                        <p><i class="fa-solid fa-text-height"></i>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat.</p>
-=======
->>>>>>> 7d1a152b25cd428510ec617a9d78c133b739602d
 
 
-<<<<<<< HEAD
-                    </div>   
-    </div></div></div></div>
-    
-=======
     <div class="recipienteProDeta">
         <div id="carouselExampleIndicators" class="carousel slide tamano-carrus" data-bs-ride="carousel">
             <div class="carousel-inner">
@@ -131,4 +76,3 @@
 
 </div>
 
->>>>>>> 7d1a152b25cd428510ec617a9d78c133b739602d
