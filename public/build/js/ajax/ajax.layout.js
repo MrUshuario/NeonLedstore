5032832255859fad53609ebf693699be97a9044f
RenzoCO <rcounac@gmail.com>
2022-03-07 10:08:01 -0500

    msj commit

--- public/build/js/ajax/ajax.layout.js
+++ public/build/js/ajax/ajax.layout.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 
 const btnSave = document.querySelector("save");
-=======
-const btnSave = document.querySelector("#save"); // borrar
->>>>>>> e84b5cd38292c73af9d4dd31a6da8c6c835af693
 $(document).ready(function() {
     $("#contra").val("");
     // boton guardar
@@ -16,7 +12,6 @@ $(document).ready(function() {
     updatePassword();
     mostrarform();
 });
-
 function data() {
     $.ajax({
         url: '/configuracion/getData',
@@ -25,27 +20,28 @@ function data() {
             //si no esta logeado enviar un mensaje que empieza con <
             if (e[0] == "<") {
             console.log("No logeado");
-            document.getElementById('Carrito').style.display = '';
-            document.getElementById('Iniciar_S').style.display = '';
+            document.getElementById('Administrar').style.display = 'none';
+            document.getElementById('Perfil').style.display = 'none';
+            document.getElementById('Cerrar_S').style.display = 'none';
             } else {
             var {data} = JSON.parse(e);
+            console.log(data);
                 if(data.cli_rol == 1){
                     //$(document).ready(function() {
                     //    $('#Perfil').text('Admin');
                     //    $('#Perfil').append('   <i class="fas fa-user"></i>');
                     //});
-                    document.getElementById('Perfil').style.display = '';
-                    document.getElementById('Cerrar_S').style.display = '';
-                    
+                    document.getElementById('Iniciar_S').style.display = 'none';
+                    document.getElementById('Carrito').style.display = 'none';
+                    document.getElementById('Administrar').style.display = 'none';
                 }if(data.cli_rol == 2){
                     var nombre = data.cli_nombre;
+                    document.getElementById('Iniciar_S').style.display = 'none';
+                    document.getElementById('Perfil').style.display = 'none';
                     $(document).ready(function() {
                         $('#Administrar').text(nombre);
                         $('#Administrar').append('   <i class="fas fa-cog"></i>');
                     });
-                    document.getElementById('Administrar').style.display = '';
-                    document.getElementById('Carrito').style.display = '';
-                    document.getElementById('Cerrar_S').style.display = '';
                 }
             }
 
