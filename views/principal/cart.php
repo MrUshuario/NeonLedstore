44e3656a1aa2486c8a1cfd4b60c1b2d14327158d
MrUsher <lenonpoma@gmail.com>
2022-03-07 12:03:13 -0500

    quite center

--- views/principal/cart.php
+++ views/principal/cart.php
@@ -6,16 +6,16 @@
 		<h1 class= "neones" style="font-family:Roboto Condensed"><i class="fa-solid fa-cart-shopping"></i> Lista de Objetos por Comprar</h1><br>
 	
 		<div class="table-responsive">
-		<center><table class="table table-striped table-hover table-light table-fixed w-100">
+		<table class="table table-striped table-hover table-light table-fixed w-100">
 		
-			<tr align="center">
+			<tr class="text-center">
 				<th>ID PRODUCTO</th><th>NOMBRE</th><th>PRECIO</th><th><i class="fa-solid fa-circle-minus"></i></th>
 			</tr>
 			<tr>
 				
 			</tr>
 
-		</table></center>
+		</table>
 		</div>
 		<div class="modal-footer">
             <button type="submit" class="btn btn-primary" id="">Guardar</button>
