73e8664f794e0e33657e68a7d06f459a03fc6486
Christian <christiand0393@gmail.com>
2022-03-07 11:54:15 -0500

    carrito

--- views/principal/cart.php
+++ views/principal/cart.php
@@ -1,17 +1,23 @@
-<div class="pt-5 page-container bg-black">
+<div class="content-principal container mt-20">
 
 	<div class="col-12 col-sm-12  border neontabla">
-		<h1 class= "neones" style="font-family:Roboto Condensed"><i class="fa-solid fa-cart-shopping"></i> Lista de Objetos por Comprar</h1>
+		<h1 class= "neones" style="font-family:Roboto Condensed"><i class="fa-solid fa-cart-shopping"></i> Lista de Objetos por Comprar</h1><br>
 	
-		<div>
-		<center><table border="2" background="white">
+		<div class="table-responsive">
+		<center><table class="table table-striped table-hover table-light table-fixed w-100">
 		
+			<tr align="center">
+				<th>ID PRODUCTO</th><th>NOMBRE</th><th>PRECIO</th><th><i class="fa-solid fa-circle-minus"></i></th>
+			</tr>
 			<tr>
-				<th>ID PRODUCTO</th><th>NOMBRE</th><th>PRECIO</th>
+				
 			</tr>
 
 		</table></center>
 		</div>
+		<div class="modal-footer">
+            <button type="submit" class="btn btn-primary" id="save">Guardar</button>
+        </div>
 	</div>
 
 </div>
