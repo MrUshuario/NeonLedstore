cce11d0119e7035c4c35305a02ccaae77e943cd9
Christian <christiand0393@gmail.com>
2022-03-07 11:28:15 -0500

    carrito de compras 40%

--- views/principal/cart.php
+++ views/principal/cart.php
@@ -1,10 +1,17 @@
-<div>
+<div class="pt-5 page-container bg-black">
+
+	<div class="col-12 col-sm-12  border neontabla">
+		<h1 class= "neones" style="font-family:Roboto Condensed"><i class="fa-solid fa-cart-shopping"></i> Lista de Objetos por Comprar</h1>
 	
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		<div>
+		<center><table border="2" background="white">
+		
+			<tr>
+				<th>ID PRODUCTO</th><th>NOMBRE</th><th>PRECIO</th>
+			</tr>
+
+		</table></center>
+		</div>
+	</div>
 
 </div>
