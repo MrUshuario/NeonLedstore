70461a4faca085c7401c039363db545abf3dd542
Christian <christiand0393@gmail.com>
2022-04-25 12:55:55 -0500

    arreglo

--- views/principal/administrar.php
+++ views/principal/administrar.php
@@ -7,18 +7,21 @@
             
                     <div class="recipient">
 
-                            <div class="contenedor-menuAdmin">
-                                <div class="sobre-admin">
-                                    <h3 class="text-white"><i class="fa-solid fa-address-card"></i> Sobre el Cliente: </h3>
-                                </div>
-                                    <ul class="menuAdmin">
-                                        <!--<a href="#"><li class="listaAdmin"><i class="icon_izq_admin fa-solid fa-user-gear"></i>  Sobre el Cliente</li></a>-->
-                                        <a href="#"><li class="listaAdmin" onclick="cambioHistorial()"><i class="icon_izq_admin fa-solid fa-boxes-stacked"></i>  Historial de Factura</li></a>
-                                        <a href="#"><li class="listaAdmin" onclick="cambioDatosPersonales()"><i class="fa-regular fa-circle-user"></i>  Datos Personales</li></a>
-                                        <a href="#"><li class="listaAdmin" onclick="cambioContrasena()"><i class="fa-solid fa-lock"></i> Cambiar Contraseña</li></a>
-                                        <a href="#"><li class="listaAdmin" onclick="verificacionCorreo()"><i class="fas fa-user-check"></i> Activar cuenta</li></a>
-                                    </ul>
+                        <div class="contenedor-menuAdmin">                  
+                            <div class="sobre-admin">
+                                <h3 class="text-white">Sobre el Cliente: </h3>
                             </div>
+                            <div id="toggle-menu" class="toggle-menu">
+                                <i class="fa-solid fa-circle-chevron-down toggle-menu_icons"></i>
+                            </div>
+                            <ul class="menuAdmin">
+                                <!-- <a href="#"><li class="listaAdmin"><i class="icon_izq_admin fa-solid fa-user-gear"></i>  Sobre el Cliente</li></a> -->
+                                <a href="#" class="listaAdmin" onclick="cambioHistorial()"><i class="fa-solid fa-boxes-stacked"></i>  Historial de Factura</li></a><br><br>
+                                <a href="#" class="listaAdmin" onclick="cambioDatosPersonales()"><i class="fa-regular fa-circle-user"></i>  Datos Personales</li></a><br><br>
+                                <a href="#" class="listaAdmin" onclick="cambioContrasena()"><i class="fa-solid fa-lock"></i> Cambiar Contraseña</li></a><br><br>
+                                <a href="#" class="listaAdmin" onclick="verificacionCorreo()"><i class="fas fa-user-check"></i> Activar cuenta</li></a>
+                            </ul>                         
+                        </div>
 
 
                         <div class="mostrar-res" id="es">
